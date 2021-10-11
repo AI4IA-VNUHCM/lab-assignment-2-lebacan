f9f5cee2340a57c947b354da5daa09dd7dadb802
lebacan <92009500+lebacan@users.noreply.github.com>
2021-10-11 19:48:57 +0700

    Add files via upload

---
+++ Exercise3.c
@@ -0,0 +1,34 @@
+/*
+3. Write a function to represent a given integer as product of prime numbers
+Ex:
+______________________________________
+| Input: 24                          |
+| Output: 2 * 2 * 2 * 3              |
+|____________________________________|
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	int testcase = atoi(argv[1]);
+	
+	//Your codes here
+	while (testcase%2 == 0)
+	{
+		printf("%d",2);
+		testcase = testcase/2;
+	}
+	for (int i = 3; i <=sqrt(testcase;i=i+2))
+	{
+		printf("%d",i);
+		testcase = testcase/i;
+		if(testcase >= i)
+		printf("* ")
+	}
+}
+if(testcase > 2)
+   printf ("%d",testcase);
+	return 0;
+}
---
+++ Exercise4.c
@@ -0,0 +1,31 @@
+/*
+4.	Receive an array of n integers, n is even. 
+Sort the first half in increasing order and second half decreasing order
+Ex:
+ ____________________________________
+| Input:  8 7 9 2 1 5 2 0            |
+| Output: 2 7 8 9 5 2 1 0            |
+|____________________________________|
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>  
+
+
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	argc--;
+	int n = argc;
+	int test_array[n],i;
+	for(i=0; i<n;i++){
+		test_array[i] = atoi(argv[i+1]);
+	}
+	//Your codes here
+	int test_array[100];
+	do{
+		printf(" input n: ");
+		scanf("%d,", n);
+	}while(n%2!=0)
+	return 0;
+}
